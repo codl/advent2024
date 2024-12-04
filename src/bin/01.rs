fc3702bb67e5d776d114ba70b4c18d1b0a15ec9b
codl <codl@codl.fr>
2024-12-04 15:58:15 +0100

    day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
 use std::io::{BufRead, BufReader};
 use std::fs::File;
 use std::str::FromStr;
+use std::hash::Hash;
 
 fn line_to_tuple(line: String) -> (i64, i64) {
     let mut split = line.split_whitespace();
@@ -14,6 +16,16 @@ fn parse_lists<R: BufRead>(reader: R) -> (Vec<i64>, Vec<i64>) {
     (left, right)
 }
 
+fn count<T: Eq+Hash>(vec: Vec<T>) -> HashMap<T, i64> {
+    let mut map : HashMap<T, i64> = Default::default();
+    for item in vec {
+        let entry = map.entry(item).or_insert_with(|| 0);
+        *entry += 1;
+    }
+
+    map
+}
+
 fn part1<R: BufRead>(reader: R) -> i64 {
     let (mut left, mut right) = parse_lists(reader);
     left.sort_unstable();
@@ -22,7 +34,9 @@ fn part1<R: BufRead>(reader: R) -> i64 {
 }
 
 fn part2<R: BufRead>(reader: R) -> i64 {
-    0
+    let (left, right) = parse_lists(reader);
+    let right_counts = count(right);
+    left.into_iter().map(|l| l * right_counts.get(&l).unwrap_or(&0)).sum()
 }
 
 const INPUT_PATH: &str = "input/01.txt";
@@ -44,7 +58,7 @@ fn part1_test() {
 
 #[test]
 fn part2_test() {
-    assert_eq!(part2(BufReader::new(TEST_INPUT.as_bytes())), 0);
+    assert_eq!(part2(BufReader::new(TEST_INPUT.as_bytes())), 31);
 }
 
 fn main() {
